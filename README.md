d9c6059f216b6b9cc446dddaa5b79786f41d1e50
yepengding <dingyepeng@gmail.com>
2022-06-21 19:42:39 +0900

    Add setup script.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # nginx-ssl
+
 Automatically set up SSL in Nginx with Docker.
+
+# Setup
+
+1. Install [docker compose](https://docs.docker.com/compose/install/#install-compose)
+2. Clone the repository
+
+```shell
+https://github.com/yepengding/nginx-ssl.git
+```
+
+3. Modify `setup.sh`
+
+- Replace `domain` with your domain name
+
+4. Assign permissions to `setup.sh`
+
+```shell
+chmod +x setup.sh
+```
+
+5. Run `setup.sh`
+
+```shell
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+domain=example.org
+
+echo "### Generating initial Ngninx config..."
+echo "server {
+    listen 80;
+    listen [::]:80;
+
+    server_name $domain www.$domain;
+    server_tokens off;
+
+    location /.well-known/acme-challenge/ {
+        root /var/www/certbot;
+    }
+
+    location / {
+        return 301 https://$domain\$request_uri;
+    }
+}" > nginx/conf/app.conf
+
+echo "### Composing Nginx and Cerbot services..."
+docker compose up -d
+
+echo "### Simulating a certificate request..."
+docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d $domain
+
+echo "### Generating SSL Nginx config..."
+echo "server {
+    listen 443 default_server ssl http2;
+    listen [::]:443 ssl http2;
+
+    server_name $domain;
+
+    ssl_certificate /etc/nginx/ssl/live/$domain/fullchain.pem;
+    ssl_certificate_key /etc/nginx/ssl/live/$domain/privkey.pem;
+    
+    location / {
+    	# ...
+    }
+}" >> nginx/conf/app.conf
+
+echo "### Creating certificates..."
+docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d $domain
+
+echo "### Restarting services..."
+docker compose restart
+
+echo "### Set up successfully!"
